8e6ce23bd7244920f654ca03a05fbe18f21c6b3d
moniraofficial <me.moniraofficial@gmail.com>
2026-07-01 16:58:26 +0600

    added proxy.js file

--- src/proxy.js
+++ src/proxy.js
@@ -21,41 +21,10 @@ return NextResponse.redirect(new URL('/login', request.url))
 }
  
 export const config = {
-  matcher: ["/add-car/:path*",
-    "/my-bookings/:path*",
-    "/my-added-cars/:path*",
-    "/api/bookings/:path*"],
+  matcher: [
+    '/dashboard/:path*',     
+    '/api/admin/:path*',      
+    '/api/users/:path*',      
+  ],
 }
 
-// import { NextResponse } from "next/server";
-
-// // 🛠️ ফিক্স: ফাংশনটির নাম middleware দিয়ে default export করা হলো
-// export default async function middleware(request) {
-//   const token = request.cookies.get("token")?.value;
-//   const { pathname } = request.nextUrl;
-
-//   const isPrivateRoute = 
-//     pathname.startsWith("/add-car") || 
-//     pathname.startsWith("/my-bookings") || 
-//     pathname.startsWith("/my-added-cars");
-
-//   const isPrivateApi = pathname.startsWith("/api/bookings");
-
-//   if ((isPrivateRoute || isPrivateApi) && !token) {
-//     if (isPrivateApi) {
-//       return NextResponse.json({ error: "Unauthorized access! No token found." }, { status: 401 });
-//     }
-//     return NextResponse.redirect(new URL("/login", request.url));
-//   }
-
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: [
-//     "/add-car/:path*",
-//     "/my-bookings/:path*",
-//     "/my-added-cars/:path*",
-//     "/api/bookings/:path*",
-//   ],
-// };
